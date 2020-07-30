c313ebe9e6a7d66994eb0ca08aabbeacbb28d13a
JigneshPansuriaDeveloper <ec.jignesh@gmail.com>
2020-07-30 23:37:16 +0530

    Interchange key value program

---
+++ Key-Value inverse.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+data=dict();
+
+
+# In[6]:
+
+
+D ={"A":1,"B":2,"C":3,"D":4,"E":5}
+
+
+# In[18]:
+
+
+for i in list(D):
+    print({D[i]:i})
+
